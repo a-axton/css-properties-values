1100bfbaeb86953563e9ebf8e8d4b880758af80d
ideadapt <kunz@ideadapt.net>
2016-08-30 16:02:46 +0200

    Resolve property values. Use es6 target platform to profit from Promise. Resolve values in property definition table per property.

--- build/index.js
+++ build/index.js
@@ -1,88 +1,142 @@
+/*
+    version => {
+        name:string // e.g. css22, css-inilne-3, css-align-3
+        subversions:[version*]
+    }
+
+    property => {
+        name:string,
+        css-version:css3, // ignore 2.1 (mostly a subset of 2.2)
+        values: [literal*,token*,valdef*]
+    }
+
+    token => {
+        value:string // border-width, length, color (resolvable to only literals)
+    }
+
+    literal => {
+        name:string // inherit, block etc
+    }
+
+    rawProperty => {
+        name:string
+        links:[string+]
+    }
+
+    rawValues => {
+        literals:[string+]
+        links:[string+]
+    }
+*/
 "use strict";
-var Version = (function () {
-    function Version(name, specNames) {
+class Version {
+    constructor(name, specNames) {
         this.name = name;
         this.specNames = specNames;
     }
-    return Version;
-}());
-var IndexProperty = (function () {
-    function IndexProperty(name, version) {
+}
+class CssProperty {
+    constructor(name, version, values) {
         this.name = name;
         this.version = version;
+        this.values = values;
     }
-    IndexProperty.createFromElement = function ($el) {
-        var el = $el.get(0);
-        var name = el.children[0].type === "text" ? el.childNodes[0].nodeValue : el.childNodes[0].childNodes[0].nodeValue;
+}
+class IndexProperty {
+    constructor(name, version, href) {
+        this.name = name;
+        this.version = version;
+        this.href = href;
+    }
+    static createFromElement($el) {
+        let el = $el.get(0);
+        let name = el.children[0].type === "text" ? el.childNodes[0].nodeValue : el.childNodes[0].childNodes[0].nodeValue;
         name = name.replace(/\s/g, "");
-        var $links = $el.find("a[href*='#propdef-']");
-        return $links.toArray().map(function (link) {
-            var version = IndexProperty.getVersionFromHref(link.attribs["href"]);
-            return new IndexProperty(name, version);
+        let $links = $el.find("a[href*='#propdef-']");
+        return $links.toArray().map((link) => {
+            var href = link.attribs["href"];
+            var version = IndexProperty.getVersionFromHref(href);
+            return new IndexProperty(name, version, href);
         });
-    };
-    IndexProperty.getVersionFromHref = function (href) {
+    }
+    static getVersionFromHref(href) {
         href = href.toLowerCase();
-        for (var _i = 0, _a = Versions.all(); _i < _a.length; _i++) {
-            var v = _a[_i];
-            for (var _b = 0, _c = v.specNames; _b < _c.length; _b++) {
-                var spec = _c[_b];
+        for (let v of Versions.all()) {
+            for (let spec of v.specNames) {
                 if (href.indexOf(spec.toLowerCase()) > -1) {
                     return v;
                 }
             }
         }
-        console.log("unknown ref:" + href);
+        //console.log("unknown ref:" + href)
         return new Version("unknown", [href]);
-    };
-    return IndexProperty;
-}());
-var cheerio = require("cheerio");
-var got = require("got");
-var Versions = (function () {
-    function Versions() {
     }
-    Versions.all = function () {
+}
+const cheerio = require("cheerio");
+const got = require("got");
+class Versions {
+    static all() {
         return [
             Versions.css2,
             Versions.css3
         ];
-    };
-    Versions.css2 = new Version("css22", ["css22"]);
-    Versions.css3 = new Version("css3", ["css-inline-3"]);
-    return Versions;
-}());
-var Runner = (function () {
-    function Runner() {
     }
-    Runner.main = function () {
+}
+Versions.css2 = new Version("css22", ["css22"]);
+Versions.css3 = new Version("css3", ["css-inline-3"]);
+class Runner {
+    static main() {
         Runner
-            .getProperties()
-            .filter(Runner.belongsTo.bind(null, [Versions.css2, Versions.css3]))
-            .forEach(function (p) { return console.log(p.name, "belongs to", p.version.name); });
-    };
-    Runner.getProperties = function () {
-        got("https://drafts.csswg.org/indexes/#properties").then(function (response) {
-            var $doc = cheerio.load(response.body);
-            var $props = $doc("#properties + div > ul.index > li");
-            var indexProperties = [];
-            $props.each(function (_, prop) {
-                var props = IndexProperty.createFromElement($doc(prop));
-                indexProperties.push.apply(indexProperties, props);
+            .getIndexProperties().then((props) => {
+            var cssProperties = props
+                .filter(Runner.belongsTo.bind(null, [Versions.css2, Versions.css3]))
+                .map((indexProperty) => {
+                return Runner.getValues(indexProperty).then((values) => {
+                    return new CssProperty(indexProperty.name, indexProperty.version, values);
+                });
             });
-            console.log(indexProperties);
-        }).catch(function (err) {
-            console.log(err);
+            return Promise.all(cssProperties).then((props) => {
+                props.forEach((p) => console.log(p.name, "belongs to", p.version.name, ", values: ", p.values.join(', ')));
+            });
+        }).catch(console.log.bind(console));
+    }
+    static getIndexProperties() {
+        return new Promise((res, rej) => {
+            got("https://drafts.csswg.org/indexes/#properties").then((response) => {
+                var $doc = cheerio.load(response.body);
+                var $props = $doc("#properties + div > ul.index > li");
+                var indexProperties = [];
+                $props.each((_, prop) => {
+                    let props = IndexProperty.createFromElement($doc(prop));
+                    indexProperties.push(...props);
+                });
+                //console.log(indexProperties.slice(0, 10))
+                res(indexProperties);
+            }).catch(rej);
         });
-        return [
-            new IndexProperty("display", Versions.css2),
-            new IndexProperty("alignment", Versions.css3)
-        ];
-    };
-    Runner.belongsTo = function (versions, property) {
-        return versions.map(function (v) { return v.name; }).indexOf(property.version.name) > -1;
-    };
-    return Runner;
-}());
+    }
+    static getValues(property) {
+        return new Promise((res, rej) => {
+            got(property.href).then((response) => {
+                var $doc = cheerio.load(response.body);
+                var $anchorDefs = $doc(`a[name="${property.name}-prop"]`);
+                var $idDefs = $doc(`#propdef-${property.name}`);
+                var $defTable;
+                if ($anchorDefs.length > 0) {
+                    $defTable = $anchorDefs.parent().siblings(".def.propdef");
+                }
+                else if ($idDefs.length > 0) {
+                    $defTable = $idDefs.parents(".def.propdef");
+                }
+                var values = $defTable.find("tr:nth-child(2)>td:last-child").text().split('|');
+                values = values.map((s) => { return s.replace(/\s/g, ""); });
+                res(values);
+            }).catch(rej);
+        });
+    }
+    static belongsTo(versions, property) {
+        return versions.map((v) => v.name).indexOf(property.version.name) > -1;
+    }
+}
 Runner.main();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,16 +1,5 @@
 
 /*
-    version:{
-        name:string // e.g. css22, css-inilne-3, css-align-3
-        subversions:[version*]
-    }
-
-    property => {
-        name:string,
-        css-version:css3, // ignore 2.1 (mostly a subset of 2.2)
-        values: [literal*,token*,valdef*]
-    }
-
     token => {
         value:string // border-width, length, color (resolvable to only literals)
     }
@@ -18,29 +7,20 @@
     literal => {
         name:string // inherit, block etc
     }
-
-    rawProperty => {
-        name:string
-        links:[string+]
-    }
-
-    rawValues => {
-        literals:[string+]
-        links:[string+]
-    }
 */
 
 class Version{
     constructor(public name:string, public specNames:Array<string>){
     }
+}
 
-    //public toString():string {
-    //    return `{ name!: ${this.name}, specNames: ${this.specNames.join(', ')}}`
-    //}
+class CssProperty{
+    constructor(public name:string, public version:Version, public values:Array<string>){
+    }
 }
 
 class IndexProperty{
-    constructor(public name:string, public version:Version){
+    constructor(public name:string, public version:Version, public href:string){
     }
 
     public static createFromElement($el:cheerio.Cheerio):Array<IndexProperty>{
@@ -49,8 +29,9 @@ class IndexProperty{
         name = name.replace(/\s/g, "");
         let $links = $el.find("a[href*='#propdef-']")
         return $links.toArray().map<IndexProperty>((link):IndexProperty => {
-            var version = IndexProperty.getVersionFromHref(link.attribs["href"])
-            return new IndexProperty(name, version)
+            var href = link.attribs["href"]
+            var version = IndexProperty.getVersionFromHref(href)
+            return new IndexProperty(name, version, href)
         })
     }
 
@@ -63,7 +44,6 @@ class IndexProperty{
                 }
             }
         }
-        console.log("unknown ref:" + href)
         return new Version("unknown", [href])
     }
 }
@@ -87,29 +67,55 @@ class Runner {
 
     public static main(){
         Runner
-            .getProperties()
-            .filter(Runner.belongsTo.bind(null, [Versions.css2, Versions.css3]))
-            .forEach((p) => console.log(p.name, "belongs to", p.version.name))
+            .getIndexProperties().then((props) => {
+                var cssProperties = props
+                    .filter(Runner.belongsTo.bind(null, [Versions.css2, Versions.css3]))
+                    .map((indexProperty) => {
+                        return Runner.getValues(indexProperty).then((values)=>{
+                            return new CssProperty(indexProperty.name, indexProperty.version, values)
+                        })
+                    })
+                return Promise.all(cssProperties).then((props:any)=>{
+                    props.forEach((p) => console.log(p.name, "belongs to", p.version.name, ", values: ", p.values.join(', ')))
+                })
+            }).catch(console.log.bind(console))
     }
 
-    public static getProperties():Array<IndexProperty>{
-        got("https://drafts.csswg.org/indexes/#properties").then((response) => {
-            var $doc = cheerio.load(response.body)
-            var $props = $doc("#properties + div > ul.index > li")
-            var indexProperties:Array<IndexProperty> = []
-            $props.each((_, prop)=>{
-                let props = IndexProperty.createFromElement($doc(prop))
-                indexProperties.push(...props)
-            })
-            console.log(indexProperties)
-        }).catch((err)=>{
-            console.log(err)
+    public static getIndexProperties():Promise<Array<IndexProperty>>{
+        return new Promise<Array<IndexProperty>>((res, rej) => {
+            got("https://drafts.csswg.org/indexes/#properties").then((response) => {
+                var $doc = cheerio.load(response.body)
+                var $props = $doc("#properties + div > ul.index > li")
+                var indexProperties:Array<IndexProperty> = []
+                $props.each((_, prop)=>{
+                    let props = IndexProperty.createFromElement($doc(prop))
+                    indexProperties.push(...props)
+                })
+                //console.log(indexProperties.slice(0, 10))
+                res(indexProperties)
+            }).catch(rej)
         })
+    }
 
-        return [
-            new IndexProperty("display", Versions.css2),
-            new IndexProperty("alignment", Versions.css3)
-        ]
+    public static getValues(property:IndexProperty):Promise<Array<string>>{
+        return new Promise<Array<string>>((res, rej) => {
+            got(property.href).then((response) => {
+                var $doc = cheerio.load(response.body)
+                var $anchorDefs = $doc(`a[name="${property.name}-prop"]`)
+                var $idDefs = $doc(`#propdef-${property.name}`)
+                var $defTable:cheerio.Cheerio;
+
+                if($anchorDefs.length > 0){
+                    $defTable = $anchorDefs.parent().siblings(".def.propdef")
+                }else if($idDefs.length > 0){
+                    $defTable = $idDefs.parents(".def.propdef")
+                }
+                
+                var values = $defTable.find("tr:nth-child(2)>td:last-child").text().split('|');
+                values = values.map((s) => { return s.replace(/\s/g, "") });
+                res(values);
+            }).catch(rej)
+        })
     }
 
     public static belongsTo(versions:Array<Version>, property:IndexProperty):boolean{
